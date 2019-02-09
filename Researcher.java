47026539d8897830ef675fb2893c11aabdb851dc
Ethreil <37450012+Ethreil@users.noreply.github.com>
2019-02-09 14:29:20 -0600

    Add files via upload

---
+++ Researcher.java
@@ -0,0 +1,105 @@
+package com.CarbonProject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Researcher {
+
+	public static double getCurrentCO2 () {
+	
+		Scanner scnr;
+		scnr = null;
+
+		try {
+			   URL url = new URL("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt");
+			   scnr = new Scanner(url.openStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		int countLines = 0;
+		while ( scnr.hasNextLine() ) {
+					++countLines;
+					scnr.nextLine();
+		} 
+		
+		scnr.reset();
+		
+		try {
+			   URL url = new URL("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt");
+			   scnr = new Scanner(url.openStream());
+			} catch (IOException ex) {	
+				ex.printStackTrace();
+			}
+		
+		for (int i = 0; i < countLines - 1; ++i) {
+			scnr.nextLine();
+		}
+		
+		scnr.next();
+		scnr.next();
+		scnr.next();
+		
+		return Double.parseDouble(scnr.next());
+		
+	}
+	
+	public static double getAvgCO2 (String yearDate) {
+		
+	double[] valuesToAverage = new double[200];
+	
+	Scanner scnr;
+	scnr = null;
+	
+	try {
+		   URL url = new URL("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt");
+		   scnr = new Scanner(url.openStream());
+		}
+		catch(IOException ex) {
+		   ex.printStackTrace(); 
+		}
+	
+	int arrayCount = 0;
+	while (scnr.hasNext()) {
+		
+		if (scnr.next().contains(yearDate)) {
+			
+			if(Integer.parseInt(scnr.next()) < 20) {
+				scnr.next();
+				valuesToAverage[arrayCount] = scnr.nextDouble();
+				++arrayCount;
+			} else {
+				valuesToAverage[arrayCount] = scnr.nextDouble();
+				++arrayCount;
+			}
+			
+		}
+		
+	}
+	
+	double totalCO2 = 0.0;
+	for (int i = 0; i < arrayCount; ++i) {
+		totalCO2 = totalCO2 + valuesToAverage[i];
+	}
+	
+	totalCO2 = totalCO2 / (double) arrayCount;
+	
+	return totalCO2;
+}
+	
+	public static double getRateOfChange (String yearOne, String yearTwo) {
+		
+		double avgCO2One = getAvgCO2(yearOne);
+		double avgCO2Two = getAvgCO2(yearTwo);
+		
+		double yearUno = Double.parseDouble(yearOne);
+		double yearDos = Double.parseDouble(yearTwo);
+		
+		double rateOfChange = (avgCO2Two - avgCO2One) / (yearDos - yearUno);
+		
+		return rateOfChange;
+	}
+	
+	
+}
---
+++ mainTester.java
@@ -0,0 +1,11 @@
+package com.CarbonProject;
+
+public class mainTester {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Between 1960 and 2018 there was a CO2 ppm increase of: " + Researcher.getRateOfChange("1960", "2018") + " per year on average");
+		
+	}
+
+}
